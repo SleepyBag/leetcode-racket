f02f8677a0ef5c38ca6d95235d3976f1a00fb662
Xue Qianming <39435859+SleepyBag@users.noreply.github.com>
2021-06-29 09:34:15 +0800

    Create 168.rkt

---
+++ 168.rkt
@@ -0,0 +1,11 @@
+(define (convert-to-title columnNumber [first #t])
+  (if (= columnNumber 0)
+      ""
+      (let ([columnNumber (sub1 columnNumber)])
+        (string-append 
+         (convert-to-title (quotient columnNumber 26))
+         (string (integer->char (+ (modulo columnNumber 26) (char->integer #\A))))
+         )
+        )
+      )
+  )
